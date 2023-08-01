33cab1cdcf8717f10a3f4f32865269dc5c9128d8
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2023-08-01 14:54:46 +0530

    Running Selenium WebDriver in Headless Mode

--- SeleniumPractice/src/main/java/org/example/HeadlessModeExample.java
+++ SeleniumPractice/src/main/java/org/example/HeadlessModeExample.java
@@ -1,4 +1,23 @@
 package org.example;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class HeadlessModeExample {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        // Set up Chrome Options
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+
+        // Initialize WebDriver with Chrome Options
+        WebDriver driver = new ChromeDriver(options);
+
+        // Load a URL to test the setup
+        driver.get("https://web-playground.ultralesson.com/");
+
+        driver.quit(); // Close the driver at the end of the program
+    }
 }
