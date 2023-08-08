ae61de84596ea6c9b54f08f2ea1cf90d69821d1d
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2023-08-08 12:27:58 +0530

    Order Confirmation & Log Out: Ensuring the Successful Shopping Experience

--- SeleniumPractice/src/main/java/org/example/PlaceOrderScript.java
+++ SeleniumPractice/src/main/java/org/example/PlaceOrderScript.java
@@ -1,11 +1,8 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
@@ -21,26 +18,43 @@ public class PlaceOrderScript {
         WebDriver driver = new ChromeDriver();
         try {
             driver.manage().window().maximize();
-
+            driver.manage().timeouts().implicitlyWait(2, java.util.concurrent.TimeUnit.SECONDS);
             driver.get("https://web-playground.ultralesson.com/");
-            driver.findElement(By.cssSelector("#shopify-section-header > sticky-header > header > div > a.header__icon.header__icon--account.link.focus-inset.small-hide")).click();
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("window.scrollBy(0, 500);");
+            driver.findElement(By.cssSelector("#shopify-section-template--15328405749981__1635852374581d2a7e > div > slider-component > ul > li:nth-child(1) > div > div.card-information > div > h3 > a")).click();
+            driver.findElement(By.cssSelector("#ProductInfo-template--15328405717213__main > variant-radios > fieldset > label:nth-child(15)")).click();
+            WebElement quantity = driver.findElement(By.className("quantity__input"));
+            quantity.click();
+            quantity.sendKeys(Keys.DELETE);
+            sleep(1000);
+            quantity.sendKeys("4");
+            driver.findElement(By.cssSelector("#product-form-template--15328405717213__main > div > button")).click();
+            String itemCount = driver.findElement(By.cssSelector("#cart-icon-bubble > div > span:nth-child(1)")).getText();
+            System.out.println("Item count in the cart is : " + itemCount);
             Wait<WebDriver> wait = new FluentWait<>(driver)
-                    .withTimeout(Duration.ofSeconds(10))
+                    .withTimeout(Duration.ofSeconds(20))
                     .pollingEvery(Duration.ofSeconds(2))
                     .ignoring(NoSuchElementException.class);
-            WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.partialLinkText("Create account")));
-            element.click();
-            driver.findElement(By.id("RegisterForm-FirstName")).sendKeys("Al");
-            driver.findElement(By.id("RegisterForm-LastName")).sendKeys("Fareed");
-            driver.findElement(By.id("RegisterForm-email")).sendKeys("alfareed@testvagrant.com");
-            driver.findElement(By.id("RegisterForm-password")).sendKeys("alfareed78@testvagrant.com");
-            driver.findElement(By.cssSelector("#create_customer > button")).click();
-            sleep(2000);
-        } catch (Exception e) {
-            System.out.println("Error...! : " + e.getMessage());
-        } finally {
-            driver.quit();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("cart-notification-form"))).click();
+            driver.findElement(By.cssSelector("#shopify-section-header > sticky-header > header > div > a.header__icon.header__icon--account.link.focus-inset.small-hide")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("Log out"))).click();
 
+
+//            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("CustomerEmail"))).sendKeys("alfareed@testvagrant.com");
+//            WebElement password = driver.findElement(By.id("CustomerPassword"));
+//            password.sendKeys("alfareed@TV781");
+//            password.sendKeys(Keys.ENTER);
+//
+//            sleep(20000);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            sleep(4000);
+            driver.quit();
         }
+
+
     }
 }
---
+++ SeleniumPractice/src/main/java/org/example/SingnUpDemo.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
+import static java.lang.Thread.sleep;
+
+public class SingnUpDemo {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.manage().window().maximize();
+
+            driver.get("https://web-playground.ultralesson.com/");
+            driver.findElement(By.cssSelector("#shopify-section-header > sticky-header > header > div > a.header__icon.header__icon--account.link.focus-inset.small-hide")).click();
+            Wait<WebDriver> wait = new FluentWait<>(driver)
+                    .withTimeout(Duration.ofSeconds(10))
+                    .pollingEvery(Duration.ofSeconds(2))
+                    .ignoring(NoSuchElementException.class);
+            WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.partialLinkText("Create account")));
+            element.click();
+            driver.findElement(By.id("RegisterForm-FirstName")).sendKeys("Al");
+            driver.findElement(By.id("RegisterForm-LastName")).sendKeys("Fareed");
+            driver.findElement(By.id("RegisterForm-email")).sendKeys("alfareed@testvagrant.com");
+            driver.findElement(By.id("RegisterForm-password")).sendKeys("alfareed78@testvagrant.com");
+            driver.findElement(By.cssSelector("#create_customer > button")).click();
+            sleep(2000);
+        } catch (Exception e) {
+            System.out.println("Error...! : " + e.getMessage());
+        } finally {
+            driver.quit();
+
+        }
+    }
+}
